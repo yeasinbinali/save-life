f11940c34a3e3c9f3c53ddc8c0ddb1a5f7674b3e
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-09-05 11:05:30 +0600

    button outlined added

--- src/components/ButtonOutlined/ButtonOutlined.jsx
+++ src/components/ButtonOutlined/ButtonOutlined.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ButtonOutlined = ({buttonName}) => {
     return (
-        <button className='btn btn-outline border-main text-main text-[white] px-5 py-2 rounded-none cursor-pointer text-[20px]'>{buttonName}</button>
+        <button className='btn btn-outline border-main text-main px-5 py-2 rounded-none cursor-pointer text-[20px]'>{buttonName}</button>
     );
 };
 
